f0b6097fa858c40d220e1f1e12ac6f8984b81a0f
CKATEPTb <taksis@ex.ua>
2023-06-18 18:16:05 +0300

    Add ImmutableVector#getDistanceAboveGround

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     id("io.papermc.paperweight.userdev").version("1.3.8")
 }
 group = "dev.ckateptb.minecraft"
-version = "1.2.3-SNAPSHOT"
+version = "1.2.4-SNAPSHOT"
 
 val rootPackage = "${project.group}.${project.name.toLowerCase()}"
 val internal = "${rootPackage}.internal"
